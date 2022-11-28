fdec6684b53af2789b1f61e11e36df476239df65
abhay-bhongade1996 <a.bhongade@braininventory.com>
2022-11-28 12:34:48 +0530

    updated about section

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
             <p className='commonp'> &#8226; I'm Currently working as a Front End Developer at Brain Inventory Indore, Madhya Pradesh  :)</p>
             <p className='commonp'> &#8226; I have around 1 year of hands-on experience in building and maintaining responsive websites, Highly skilled in HTML,CSS,Javascipt and Bootstrap and alongside modern Frameworks and libraries.</p>
             <p className='commonp'>&#8226; I love to combine world of logic and creative design to make eye-catching,accessible and user friendy websites and applications.</p>
-            <p className='commonp'>&#8226;I'm excited to make the leap and continue refining my skills with the right company.</p>
+            <p className='commonp'>&#8226; I'm excited to make the leap and continue refining my skills with the right company.</p>
           </div>
         </div>
        
